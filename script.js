7c9fdf9b6ce7c4eea330efd80b029505264d3b6f
Saurav Panda <38997868+mrsauravpanda@users.noreply.github.com>
2019-12-19 15:56:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,14 @@
     $("body").toggleClass("night");
   })})
 
+  $(document).ready(function(){
+    var now=new Date();
+    var hour=now.getHours();
+    console.log(hour);
+    if(hour < 4 && hour > 18){
+      $(".toggle").toggleClass("active");
+      $("body").toggleClass("night");
+    }
+  })
+  
+
